366f3881a19e3b57d48c83dadeb6c64ade96081e
Junior Paula <luke.junnior@icloud.com>
2023-07-09 09:34:39 -0300

    refactor: change database postgres to mysql

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,24 +1,13 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+CREATE DATABASE IF NOT EXISTS knowledge;
+USE knowledge;
 
-SET default_tablespace = '';
+DROP TABLE IF EXISTS users;
 
-SET default_table_access_method = heap;
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    name character varying(255),
-    email character varying(255),
-    password character varying(60),
-    admin integer,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
-);
+CREATE TABLE users(
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    email varchar(50) not null unique,
+    password varchar(100) not null,
+    admin boolean default false,
+    createdAt timestamp default current_timestamp()
+)ENGINE=INNODB;
