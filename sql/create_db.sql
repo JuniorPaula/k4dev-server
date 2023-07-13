c7e5528b68b4c2573192d5d78b92c86fb1df727e
Junior Paula <luke.junnior@icloud.com>
2023-07-13 19:04:57 -0300

    chore: add script to create categories table

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS knowledge;
 USE knowledge;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
 
 CREATE TABLE users(
     id int auto_increment primary key,
@@ -11,3 +12,11 @@ CREATE TABLE users(
     admin boolean default false,
     createdAt timestamp default current_timestamp()
 )ENGINE=INNODB;
+
+CREATE TABLE categories(
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    parent_id int,
+    FOREIGN KEY (parent_id) REFERENCES categories(id),
+    createdAt timestamp default current_timestamp()
+) ENGINE=INNODB;
